b6a8c531fd48a2b2492d38d3743e7b726bbcd281
Joel Sommers <jsommers@colgate.edu>
2017-04-04 11:39:31 -0400

    Partial refactoring done; some more debug to do

--- monitors/rtt_monitor.py
+++ monitors/rtt_monitor.py
@@ -16,6 +16,9 @@ from switchyard import pcapffi
 from localnet import *
 
 
+# destination port for TCP and UDP probes
+DESTPORT = 44444
+
 def _create_decoder():
     _dlt_to_decoder = {}
     _dlt_to_decoder[pcapffi.Dlt.DLT_EN10MB] = lambda raw: Packet(raw, first_header=Ethernet)
@@ -31,37 +34,178 @@ def _create_decoder():
 
 decode_packet = _create_decoder()
 
-
-_protomap = {
-    'icmp': (IPProtocol.ICMP,ICMP),
-    'tcp': (IPProtocol.TCP,TCP),
-    'udp': (IPProtocol.UDP,UDP),
-}
-
-
 class ProbeDirection(IntEnum):
     Outgoing = 0
     Incoming = 1
 
 
-class ProbeHelper(object):
-    pass
+class ArpCache(object):
+    TIMEOUT = 600
 
-class ICMPHelper(ProbeHelper):
+    def __init__(self):
+        self._cache = {}
+        for ipaddr,ethaddr in read_system_arp_cache().items():
+            self.update(ipaddr, ethaddr)
+
+    def lookup(self, ipaddr):
+        if ipaddr in self._cache:
+            ethaddr, ts = self._cache[ipaddr]
+            tdiff = time() - ts
+            if tdiff < ArpCache.TIMEOUT:
+                return ethaddr
+        return None
+
+    def update(self, ipaddr, ethaddr):
+        self._cache[ipaddr] = (ethaddr, time())
+
+
+class ProbeHelper(object):
+    @staticmethod
+    def reconstruct(raw):
+        p = Packet()
+        ip = IPv4()
+        rawremain = ip.from_bytes(raw)
+        p += ip
+        return p,raw
+
+    @staticmethod
+    def make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl):
+        p = Ethernet(src=ethsrc) + \
+            IPv4(src=ipsrc, dst=ipdst, protocol=proto, ttl=maxttl)
+        return p
+
+    @staticmethod
+    def fill_in(p, ethdst, ttl):
+        p[Ethernet].dst = ethdst
+        p[IPv4].ttl = ttl
+
+
+class ICMPProbeHelper(ProbeHelper):
+    '''
+    identifier: ICMP echo ident
+    sequence: ICMP echo seq
+    original ttl: IP id
+    '''
     name = 'icmp'
     klass = ICMP
     proto = IPProtocol.ICMP
+    pcapfilter = '(icmp[icmptype] == icmp-echo or icmp[icmptype] == icmp-echoreply or icmp[icmptype] == icmp-timxceed) or arp'
+
+    @staticmethod
+    def reconstruct(raw):
+        p,raw = ProbeHelper.reconstruct(raw)
+        icmp = ICMP()
+        icmp.from_bytes(raw)
+        p += icmp
+        return p
+
+    @staticmethod
+    def decode_carcass(p):
+        seq = p[ICMP].icmpdata.sequence
+        ident = p[ICMP].icmpdata.identifier
+        origttl = p[IPv4].ipid & 0xff
+        return seq, ident, origttl
+
+    @staticmethod
+    def make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl, ident, dport=44444):
+        p = ProbeHelper.make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl)
+        p += ICMP(icmptype=ICMPType.EchoRequest, identifier=ident)
+        return p
+
+    @staticmethod
+    def fill_in(p, ethdst, ttl, seq):
+        ProbeHelper.fill_in(p, ethdst, ttl)
+        p[IPv4].ipid = ttl 
+        p[ICMP].icmpdata.sequence = seq
 
 
-class TCPHelper(ProbeHelper):
+class TCPProbeHelper(ProbeHelper):
+    '''
+    identifier: TCP source port
+    sequence: TCP sequence (it is within the first 8 bytes of TCP header)
+    original ttl: IP id
+    '''
     name = 'tcp'
     klass = TCP
     proto = IPProtocol.TCP
+    pcapfilter = 'icmp[icmptype]==icmp-timxceed or arp or dst port {}'.format(DESTPORT)
 
-class UDPHelper(ProbeHelper):
+    @staticmethod
+    def reconstruct(raw):
+        p,raw = ProbeHelper.reconstruct(raw)
+        tcp = TCP()
+        newraw = raw + (20 - len(raw))*b'\x00'
+        tcp.from_bytes(newraw)
+        p += tcp
+        return p
+
+    @staticmethod
+    def decode_carcass(p):
+        seq = p[TCP].seq
+        ident = p[TCP].src
+        origttl = p[IPv4].ipid & 0xff
+        return seq, ident, origttl
+
+    @staticmethod
+    def make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl, ident, dport=DESTPORT):
+        p = ProbeHelper.make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl)
+        p += TCP(src=ident, dst=dport, window=228)
+        p[TCP].SYN = 1
+        return p
+
+    @staticmethod
+    def fill_in(p, ethdst, ttl, seq):
+        ProbeHelper.fill_in(p, ethdst, ttl)
+        p[IPv4].ipid = ttl 
+        p[TCP].seq = seq
+
+
+class UDPProbeHelper(ProbeHelper):
+    '''
+    identifier: UDP source port
+    sequence: IP id
+    original ttl: UDP length (+ pad the packet out)
+    '''
     name = 'udp'
     klass = UDP
     proto = IPProtocol.UDP
+    pcapfilter = 'icmp[icmptype]==icmp-timxceed or arp or dst port {}'.format(DESTPORT)
+
+    @staticmethod
+    def reconstruct(raw):
+        p,raw = ProbeHelper.reconstruct(raw)
+        udp = UDP()
+        udp.from_bytes(raw)
+        p += udp
+        return p
+
+    @staticmethod
+    def decode_carcass(p):
+        seq = p[IPv4].ipid
+        ident = p[UDP].src
+        origttl = p[UDP]._len
+        return seq, ident, origttl
+
+    @staticmethod
+    def make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl, ident, dport=DESTPORT):
+        p = ProbeHelper.make_packet_template(ethsrc, ipsrc, ipdst, proto, maxttl)
+        p += UDP(src=ident, dst=dport)
+        p += RawPacketContents() # place holder for raw data
+        return p
+
+    @staticmethod
+    def fill_in(p, ethdst, ttl, seq):
+        ProbeHelper.fill_in(p, ethdst, ttl)
+        p[IPv4].ipid = seq
+        xdata = RawPacketContents(b'\x01' * ttl)
+        p[-1] = xdata
+
+
+_protomap = {
+    'icmp': ICMPProbeHelper,
+    'tcp': TCPProbeHelper,
+    'udp': UDPProbeHelper,
+}
 
 
 # FIXME: doesn't yet handle sequence wrap when compiling results
@@ -75,30 +219,25 @@ class RTTProbeSource(DataSource):
     def __init__(self, interface, probetype, proto, maxttl, allhops, dest):
         DataSource.__init__(self)
         self._interface = interface
+        self._ifinfo = get_interface_info(self._interface)
         self._probetype = probetype
+        self._probehelper = _protomap[proto]
 
         if self._probetype == 'ping':
             self._maxttl = 64
-            self._probeprotostr = 'icmp'
-            self._probeproto = IPProtocol.ICMP
-            self._probeprotocls = ICMP
             self._probe_all_hops = False
         elif self._probetype == 'hoplimited':
-            self._probeprotostr = proto
-            self._probeproto = _protomap[proto][0]
-            self._probeprotocls = _protomap[proto][1]
             self._maxttl = maxttl  # only meaningful for hop-limited probes
             self._probe_all_hops = allhops # only meaningful for hop-limited probes
-            self._destport = 44444 # for UDP and TCP probes
 
         self._dest = IPv4Address(dest)
-        self._ifinfo = None
-        self._lookup_nexthop()
+        self._nexthopip = self._lookup_nexthop() 
 
         self._probeseq = 1
+        self._probewrap = 65536
         self._num_probes_sent = self._num_probes_recv = 0
 
-        self._arp_cache = read_system_arp_cache()
+        self._arp_cache = ArpCache()
         self._arp_queue = asyncio.Queue()
         self._probe_queue = asyncio.Queue()
 
@@ -106,20 +245,20 @@ class RTTProbeSource(DataSource):
             self._sendsock = None
         self._log = logging.getLogger('mm')
 
-        self._pktident = os.getpid()%65536
+        self._pktident = os.getpid()% 65536
 
         self._seqhash = {
             ProbeDirection.Incoming: {},
             ProbeDirection.Outgoing: {},
         }
 
-        self._construct_packet_template()
+        ifinfo = self._ifinfo[self._interface]
 
-        if self._probeprotostr == 'icmp':
-            filt = '(icmp[icmptype] == icmp-echo or icmp[icmptype] == icmp-echoreply or icmp[icmptype] == icmp-timxceed) or arp'
-        else:
-            filt = 'icmp[icmptype]==icmp-timxceed or arp or dst port {}'.format(self._destport)
-        self._setup_port(interface, filt)
+        self._pkttemplate = self._probehelper.make_packet_template(ifinfo.ethsrc, 
+            ifinfo.ipsrc.ip, self._dest, self._probehelper.proto, 
+            self._maxttl, self._pktident, dport=DESTPORT)
+
+        self._setup_port(interface, self._probehelper.pcapfilter)
 
         self._monfut = asyncio.Future()
         asyncio.ensure_future(self._ping_collector(self._monfut))
@@ -171,7 +310,6 @@ class RTTProbeSource(DataSource):
         asyncio.get_event_loop().add_reader(p.fd, self._packet_arrival_callback)
 
     def _lookup_nexthop(self):
-        self._ifinfo = get_interface_info(self._interface)
         routes = get_routes(self._ifinfo)
         # debugging: dump out all interface and route info
         # for intf in self._ifinfo:
@@ -183,9 +321,9 @@ class RTTProbeSource(DataSource):
         except KeyError:
             raise RuntimeException("No route to destination {} for network monitoring".format(self._dest))
         if str(nh.nexthop) == '0.0.0.0': # address on same subnet
-            self._nexthopip = self._dest
+            return self._dest
         else:
-            self._nexthopip = nh.nexthop
+            return nh.nexthop
 
     def __call__(self):
         asyncio.ensure_future(self._emitprobe(self._dest))            
@@ -215,56 +353,21 @@ class RTTProbeSource(DataSource):
                         direction = ProbeDirection.Incoming
                     self._probe_queue.put_nowait((ts,seq,pkt[IPv4].src,pkt[IPv4].ttl,direction))
                 elif pkt[ICMP].icmptype == ICMPType.TimeExceeded:
-                    p = self._reconstruct_packet(pkt[ICMP].icmpdata.data)
+                    p = self._probehelper.reconstruct(pkt[ICMP].icmpdata.data)
                     self._log.debug("Packet carcass: {}".format(p))
-                    if p.has_header(self._probeprotocls):
-                        seq, ident = self._decode_packet_carcass(p)
+                    if p.has_header(self._probehelper.klass):
+                        seq, ident = self._probehelper.decode_carcass(p)
                         if ident == self._pktident:
                             origttl = p[IPv4].ipid & 0xff
                             self._probe_queue.put_nowait((ts,seq,pkt[IPv4].src,origttl,ProbeDirection.Incoming))
 
             # identify our outgoing TCP or UDP probe packet.  ICMP is caught
             # in prevous elif
-            elif pkt.has_header(self._probeprotocls): 
-                seq,ident = self._decode_packet_carcass(pkt)
+            elif pkt.has_header(self._probehelper.klass): 
+                seq,ident = self._decode_carcass(pkt)
                 if ident == self._pktident:
                     origttl = pkt[IPv4].ipid & 0xff
                     self._probe_queue.put_nowait((ts,seq,pkt[IPv4].src,origttl,ProbeDirection.Outgoing))
-
-    def _reconstruct_packet(self, raw):
-        p = Packet()
-        ip = IPv4()
-        rawremain = ip.from_bytes(raw)
-        p += ip
-        if ip.protocol == IPProtocol.ICMP:
-            icmp = ICMP()
-            icmp.from_bytes(rawremain)
-            p += icmp
-        elif ip.protocol == IPProtocol.UDP:
-            udp = UDP()
-            udp.from_bytes(rawremain)
-            p += udp
-        elif ip.protocol == IPProtocol.TCP:
-            tcp = TCP()
-            newraw = rawremain + (20 - len(rawremain))*b'\x00'
-            tcp.from_bytes(newraw)
-            p += tcp
-        return p
-
-    def _decode_packet_carcass_icmp(self, p):
-        seq = p[ICMP].icmpdata.sequence
-        ident = p[ICMP].icmpdata.identifier
-        return seq, ident
-
-    def _decode_packet_carcass_tcp(self, p):
-        seq = p[IPv4].ipid
-        ident = p[TCP].src
-        return seq, ident
-
-    def _decode_packet_carcass_udp(self, p):
-        seq = p[IPv4].ipid
-        ident = p[UDP].src
-        return seq, ident
 
     def _send_packet(self, intf, pkt):
         self._log.debug("Sending on {}: {}".format(intf, pkt))
@@ -274,72 +377,48 @@ class RTTProbeSource(DataSource):
             self._pcap.send_packet(pkt.to_bytes()) 
 
     async def _do_arp(self, ethsrc, ipsrc, dst, intf):
-        if dst in self._arp_cache:
+        ethdst = self._arp_cache.lookup(dst)
+        if ethdst is not None:
             self._log.debug("ARP cache hit")
-            return self._arp_cache[dst]
-        self._log.debug("ARP cache miss; sending req for {} ({})".format(dst, type(dst)))
-        arpreq = create_ip_arp_request(ethsrc, ipsrc, dst)
-        fareth = "00:00:00:00:00:00"
-        self._send_packet(intf, arpreq)
-        while True:
+            return ethdst
+
+        self._log.debug("ARP cache miss; sending req for {} ({})".format(dst))
+        last_send = 0
+        left_to_send = 5 # 5 attempts, wait 1 sec each
+        ethdst = None
+
+        while left_to_send > 0:
+            now = time()
+            if now - last_send > 1:
+                self._send_packet(intf, 
+                    create_ip_arp_request(ethsrc, ipsrc, dst))
+                last_send = now
+                left_to_send -= 1
+
             try:
                 ethaddr,ipaddr = await self._arp_queue.get()
             except asyncio.CancelledError:
                 break
-            self._arp_cache[IPv4Address(ipaddr)] = EthAddr(ethaddr)
+
+            self._arp_cache.update(IPv4Address(ipaddr), EthAddr(ethaddr))
+
             if ipaddr == dst:
+                ethdst = ethaddr
                 break
-        return ethaddr
 
-    def _construct_packet_template(self):
-        '''
-        Construct a packet template for sending.  Things yet to be filled in:
-            - Ethernet.dst
-            - IPv4.ttl (possibly)
-            - Probe sequence
-            ... done in _fill_in_pkt_details
-        '''
-        thisintf = self._ifinfo[self._interface]
-        self._pkttemplate = \
-            Ethernet(src=thisintf.ethsrc) + \
-            IPv4(src=thisintf.ipsrc.ip, dst=self._dest, protocol=self._probeproto, 
-                ttl=self._maxttl)
-
-        if self._probeproto == IPProtocol.ICMP:
-            l4 = ICMP(icmptype=ICMPType.EchoRequest,
-                      identifier=self._pktident)
-        elif self._probeproto == IPProtocol.TCP:
-            l4 = TCP(src=self._pktident, dst=self._destport, window=228)
-            l4.SYN = 1
-        elif self._probeproto == IPProtocol.UDP:
-            l4 = UDP(src=self._pktident, dst=self._destport)
-        self._pkttemplate += l4
-
-    def _fill_in_pkt_details_icmp(self, ethdst, ttl, seq):
-        self._pkttemplate[Ethernet].dst = ethdst
-        self._pkttemplate[IPv4].ttl = ttl
-        self._pkttemplate[IPv4].ipid = ttl
-        self._pkttemplate[ICMP].icmpdata.sequence = seq
-
-    def _fill_in_pkt_details_tcp(self, ethdst, ttl, seq):
-        self._pkttemplate[Ethernet].dst = ethdst
-        self._pkttemplate[IPv4].ttl = ttl
-        self._pkttemplate[IPv4].ipid = ttl
-        self._pkttemplate[TCP].seq = seq
-
-    def _fill_in_pkt_details_udp(self, ethdst, ttl, seq):
-        self._pkttemplate[Ethernet].dst = ethdst
-        self._pkttemplate[IPv4].ttl = ttl
-        self._pkttemplate[IPv4].ipid = seq
+        return ethdst
 
     async def _emitprobe(self, dst):
         thisintf = self._ifinfo[self._interface]
         self._num_probes_sent += 1
 
         try:
-            dsteth = await self._do_arp(thisintf.ethsrc, thisintf.ipsrc.ip, 
+            ethdst = await self._do_arp(thisintf.ethsrc, thisintf.ipsrc.ip, 
                 self._nexthopip, self._interface)
         except asyncio.CancelledError:
+            return
+
+        if ethdst is None:
             return
 
         seq = self._probeseq
@@ -350,13 +429,12 @@ class RTTProbeSource(DataSource):
             end_ttl = 0
 
         for ttl in range(start_ttl, end_ttl, -1):
-            self._fill_in_pkt_details(dsteth, ttl, seq)
+            self._probehelper.fill_in(self._pkttemplate, ethdst, ttl, seq)
             self._send_packet(self._interface, self._pkttemplate)
 
         self._probeseq += 1
-        if self._probeseq == 65536:
+        if self._probeseq == self._probewrap:
             self._probeseq = 1
-
 
     async def _ping_collector(self, fut):
         def store_result(seq, pcapsend, pcaprecv):
@@ -401,17 +479,6 @@ class RTTProbeSource(DataSource):
         self._add_result = resultscontainer.add_result
         self._add_metadata = metamaster.add_metadata
 
-        # setup fill-in and decode pkt handlers
-        if self._probeproto == IPProtocol.ICMP:
-            self._decode_packet_carcass = self._decode_packet_carcass_icmp
-            self._fill_in_pkt_details = self._fill_in_pkt_details_icmp
-        elif self._probeproto == IPProtocol.TCP:
-            self._decode_packet_carcass = self._decode_packet_carcass_tcp
-            self._fill_in_pkt_details = self._fill_in_pkt_details_tcp
-        if self._probeproto == IPProtocol.UDP:
-            self._decode_packet_carcass = self._decode_packet_carcass_udp
-            self._fill_in_pkt_details = self._fill_in_pkt_details_udp
-
     def cleanup(self):
         # close pcap devices and get stats from them
         pcapstats = {}
@@ -425,7 +492,7 @@ class RTTProbeSource(DataSource):
             self._log.info("Closing rawsock for sending on {}".format(self._interface))
             self._sendsock.close()
 
-        dconfig = { 'protocol':self._probeprotostr,
+        dconfig = { 'protocol':self._probehelper.name,
                     'probetype':self._probetype,
                     'dest':str(self._dest),
                     'total_probes_emitted': self._num_probes_sent,
