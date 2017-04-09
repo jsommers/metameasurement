ec375ac165ac1bb8b36537087b5e0b0a840bb969
Joel Sommers <jsommers@colgate.edu>
2017-04-08 20:44:01 -0400

    parameterize stuff

--- Benchmarks/loadTests.sh
+++ Benchmarks/loadTests.sh
@@ -6,21 +6,26 @@
 # NB: assumes that pyvenv is sourced correctly into current shell 
 # when this is executed
 
+DURATION="3600"
+INTF="eth0"
+TARGET="10.0.1.1"
+SLEEP="300"
+
 # expt with artificial cpu load
 ./LoadGenerator/runLoad.sh c &
-python3 metameasurement.py -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=eth0:type=ping:dest=10.0.1.1 -c "sleep 3600" -f load_1h_cpuload -l &
+python3 metameasurement.py -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$TARGET -c "sleep $DURATION" -f load_1h_cpuload -l &
 killall python3
-sleep 300
+sleep $SLEEP
 
 # expt with artificial mem load
 ./LoadGenerator/runLoad.sh m &
-python3 metameasurement.py -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=eth0:type=ping:dest=10.0.1.1 -c "sleep 3600" -f load_1h_memload -l &
+python3 metameasurement.py -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$TARGET -c "sleep $DURATION" -f load_1h_memload -l &
 killall python3
-sleep 300
+sleep $SLEEP
 
 # expt with artificial disk/io load
 ./LoadGenerator/runLoad.sh m &
-python3 metameasurement.py -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=eth0:type=ping:dest=10.0.1.1 -c "sleep 3600" -f load_1h_ioload -l &
+python3 metameasurement.py -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$TARGET -c "sleep $DURATION" -f load_1h_ioload -l &
 killall python3
-sleep 300
+sleep $SLEEP
 
