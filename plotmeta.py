c8029a00e49c9c5ca1fc18b330f7d71a49cb9f20
Joel Sommers <jsommers@colgate.edu>
2017-04-10 11:30:23 -0400

    rewrite of plotmeta; create mapping of keys to datalists to simplify handling of differences between rtt monitors and others

--- plotmeta.py
+++ plotmeta.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import argparse
 from statistics import mean, stdev, median
@@ -8,27 +9,24 @@ import os.path
 import matplotlib.pyplot as plt
 from cycler import cycler
 
-def _gather_ts(metad, k):
-    group, key = k.split(":")
-    tsList = []
-    valList = []
-    for ts, val in metad['monitors'][group]:
-        if key in val.keys():
-            tsList.append(ts)
-            valList.append(val[key])
-    return tsList, valList
+def _gather_ts(rawdlist, key):
+    ts = [ ts for ts,_ in rawdlist ] 
+    vals = [ xd[key] for ts,xd in rawdlist ]
+    return ts, vals
 
-def _gather_ts_rtt(metad, k):
-    group, key = k.split(":")
-    xlist = metad['monitors'][group][key]
-    tsVal = [ts for ts, xd in xlist]
-    rtt = [ xd['recv'] - xd['send'] for ts,xd in xlist if not isinf(xd['recv']) and not isinf(xd['send']) ]
-    if rtt:
-        return tsVal, rtt
-    else:
-        return tsVal, [None for i in range(len(tsVal))]
+def _gather_ts_rtt(rawdlist, xkey):
+    tsval = []
+    rtt = []
+    for i in range(len(rawdlist)): 
+        ts, xd = rawdlist[i]
+        rval = xd['recv'] - xd['send']
+        if isinf(rval) or rval < 0:
+            continue
+        tsval.append(ts)
+        rtt.append(rval)
+    return tsval, rtt  
 
-def plotItems(inbase, metadata, keys):
+def plotItems(inbase, datamap, keys):
     f, axarr = plt.subplots(figsize=(len(keys)*4,len(keys)*4), nrows=len(keys), ncols=1, sharex=True, squeeze=False)
     color_cycle = cycler(c=['r', 'g', 'b'])
     ls_cycle = cycler('ls', ['-.', '--', '-', ':'])
@@ -41,55 +39,88 @@ def plotItems(inbase, metadata, keys):
     for idx, key in enumerate(keys):
         ts = []
         data = []
-        if key.startswith('rtt'):
-            ts, data = _gather_ts_rtt(metadata, key)
+        rawdlist = datamap[key]
+        dkey = key.split(':')[-1]
+        if key.endswith('rtt'):
+            ts, data = _gather_ts_rtt(rawdlist, dkey)
         else:
-            ts, data = _gather_ts(metadata, key)
+            ts, data = _gather_ts(rawdlist, dkey)
 
-        axarr[idx,0].plot(ts, data, label=key, **styles[idx])
+        axarr[idx,0].plot(ts, data, label=dkey, **styles[idx])
         axarr[idx,0].grid()
-        axarr[idx,0].set_ylabel(key)
+        axarr[idx,0].set_ylabel(dkey)
     axarr[len(keys)-1,0].set_xlabel("Time (s)")
 
     plt.savefig("{}.png".format(inbase))
 
-def plotGroups(inbase, metadata, keyList):
-    for key in metadata['monitors'].keys():
-        for rKey in keyList:
-            if key in rKey and rKey!='rtt':
-                l = metadata['monitors'][key]
-                ts, obsdict = l.pop(0)
-                newL = [rKey+":"+k for k in obsdict.keys()]
-                plotItems(inbase+"_"+rKey, metadata, newL)
-            if key.startswith(rKey) and rKey=='rtt':
-                newL = []
-                for rttkey in metadata['monitors'][key].keys():
-                    if rttkey.startswith('ttl') or rttkey == 'ping':
-                        newL.append(key+":"+rttkey)
-                plotItems(inbase+"_"+rKey, metadata, newL)
+def plotGroups(inbase, datamap, keyList):
+    for k in keyList:
+        matchitems = [] 
+        matchkey = "{}:".format(k)
+        for dkey in datamap.keys():
+            if dkey.startswith(matchkey):
+                matchitems.append(dkey)         
+        plotItems('_'.join((inbase, k)), datamap, matchitems)
 
-def plotAll(inbase, metadata):
-    groups = _dump_keys(metadata)
-    plotGroups(inbase, metadata, groups)
+def plotAll(inbase, datamap):
+    groups = _dump_keys(datamap)
+    plotGroups(inbase, datamap, groups)
 
-def _dump_keys(metadata):
-    keyList = []
-    for key in metadata['monitors'].keys():
-        if key.startswith('rtt'):
-            keyList.append('rtt')
+def _dump_keys(datamap, dumpfull=False):
+    rv = []
+    for k in datamap.keys():
+        if dumpfull:
+            print(k)
         else:
-            keyList.append(key)
-    return keyList
+            kbase = k.split(':')
+            if kbase[0] not in rv:
+                rv.append(kbase[0])
+    return rv
+
+def _get_subkeys(xdata, isrtt):
+    keymap = {}
+    if isrtt:
+        assert(isinstance(xdata, dict))
+        for k in xdata.keys():
+            if k == 'ping' or k.startswith('ttl'):
+                xlist = xdata[k]
+                ts, xd = xlist[0]
+                for subkey in xd.keys():
+                    xkey = '{}:{}'.format(k, subkey)
+                    keymap[xkey] = xlist
+    else:
+        assert(isinstance(xdata, list))
+        ts, xd = xdata[0]
+        for xkey in xd.keys():
+            keymap[xkey] = xdata
+    return keymap
+
+def _make_key_map(metadata):
+    xmap = {}
+    xrtt = 0
+    for basekey in metadata['monitors'].keys():
+        rttmon = False
+        if basekey.startswith('rtt'):
+            keyname = 'rttmon{}'.format(xrtt)
+            xrtt += 1
+            rttmon = True
+        else:
+            keyname = basekey
+        for sk, xli in  _get_subkeys(metadata['monitors'][basekey], rttmon).items():
+            xmap['{}:{}'.format(keyname, sk)] = xli
+    return xmap
 
 def main():
     parser = argparse.ArgumentParser(description='Plotting script - v2')
-    parser.add_argument('-i', '--item', dest='items', action='append', \
+    parser.add_argument('-i', '--item', dest='items', action='append', 
                         help='Include individual item in the plot, e.g., monitors:cpu:idle')
-    parser.add_argument('-g', '--group', dest='groups', action='append', \
+    parser.add_argument('-g', '--group', dest='groups', action='append', 
                         help='Include a group of items in the plot, e.g., \
                         rtt includes rtt and seq')
-    parser.add_argument('-a', '--all', action='store_true', \
+    parser.add_argument('-a', '--all', action='store_true', 
                         help='Plot all groups in separate subplots.')
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='List all data keys that can be plotted')
     parser.add_argument('jsonmeta', nargs=1)
     args = parser.parse_args()
 
@@ -99,19 +130,21 @@ def main():
 
     inbase, ext = os.path.splitext(infile)
 
+    datamap = _make_key_map(meta)
+
     if args.items:
         print("Plotting items: {0}".format(args.items))
-        plotItems(inbase+"_items", meta, args.items)
+        plotItems(inbase+"_items", datamap, args.items)
     elif args.groups:
         print("Plotting groups: {0}".format(args.groups))
-        plotGroups(inbase, meta, args.groups)
+        plotGroups(inbase, datamap, args.groups)
     elif args.all:
         print("Plotting all items in all groups.")
-        plotAll(inbase, meta)
+        plotAll(inbase, datamap)
+    elif args.list:
+        _dump_keys(datamap, dumpfull=True)
     else:
-        print("Must include at least one group or one item from the group to plot.  Here are valid groups:")
-        items = _dump_keys(meta)
-        print(items)
+        print("Must include at least one group or one item from the group to plot.  Here are valid groups: {}".format(', '.join(_dump_keys(datamap))))
         sys.exit(-1)
 
 if __name__ == '__main__':
