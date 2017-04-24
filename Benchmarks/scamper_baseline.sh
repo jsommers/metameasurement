3291ed853ac2025f86b472e47a125b352722f914
Joel Sommers <jsommers@colgate.edu>
2017-04-24 08:44:30 -0400

    fix backtick

--- Benchmarks/scamper_baseline.sh
+++ Benchmarks/scamper_baseline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-HOST=`hostname
+HOST=`hostname`
 DT=`date +"%F-%T"`
 scamper -c "ping -P icmp-echo -c 30 -s 64" -p 10 -w 5 -f Benchmarks/targets.txt -M ${HOST} -O warts -o ${HOST}_${DT}_baseline.warts
