761e3ca45101882251af70c473fdb525bc2d3107
Joel Sommers <jsommers@colgate.edu>
2017-04-24 08:34:04 -0400

    add date/time to scamper baseline script

--- Benchmarks/scamper_baseline.sh
+++ Benchmarks/scamper_baseline.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -x
-scamper -c "ping -P icmp-echo -c 30 -s 64" -p 10 -w 5 -f Benchmarks/targets.txt -M `hostname` -O warts -o `hostname`_baseline.warts
+HOST=`hostname
+DT=`date +"%F-%T"`
+scamper -c "ping -P icmp-echo -c 30 -s 64" -p 10 -w 5 -f Benchmarks/targets.txt -M ${HOST} -O warts -o ${HOST}_${DT}_baseline.warts
