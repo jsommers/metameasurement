771449b688743b7ae3cf731ed08bba09d43eb3fb
Joel Sommers <jsommers@colgate.edu>
2017-04-28 13:43:33 -0400

    clean up baseline script for running again

--- Benchmarks/baseline.sh
+++ Benchmarks/baseline.sh
@@ -1,14 +1,25 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 INTF=eth0
 DEST=10.42.42.3
 DURATION=3600
-CPUAFF="-C0"
+NCPU=`cat /proc/cpuinfo | grep '^processor' | wc -l`
+CPUPIN=$(($NCPU-1))
+CPUAFF="-C${CPUPIN}"
+SLEEP=10
 
-# all with default 1s sampling interval, or avg 1 probe/sec
-python3 metameasurement.py $CPUAFF -Mcpu -c "sleep $DURATION" -F overhead_1hcpuonly -l
-#python3 metameasurement.py $CPUAFF -Mmem -c "sleep $DURATION" -F overhead_1hmemonly -l
-#python3 metameasurement.py $CPUAFF -Mio -c "sleep $DURATION" -F overhead_1hioonly -l
-#python3 metameasurement.py $CPUAFF -Mnetstat -c "sleep $DURATION" -F overhead_1hnetstatonly -l
-python3 metameasurement.py $CPUAFF -Mrtt:interface=$INTF:type=ping:dest=$DEST -c "sleep $DURATION" -F overhead_1hrttonly -l
-python3 metameasurement.py $CPUAFF -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$DEST -c "sleep $DURATION" -F overhead_1hallmon -l
+for MON in cpu mem io netstat cpu:interval=5 rtt:interface=${INTF}:type=ping:dest=${DEST} rtt:interface=${INTF}:type=hoplimited:maxttl=1:dest=8.8.8.8 ; do
+    MONNAME=`echo $MON | perl -pe 's/[:\.]/_/g' | perl -pe 's/=//g'`
+    OUTNAME=baseline_${DURATION}_${MONNAME}
+    echo ${OUTNAME}
+    date
+    # python3 metameasurement.py $CPUAFF -M${MON} -c "sleep $DURATION" -F ${OUTNAME} -l
+    #sleep ${SLEEP}
+done
+
+OUTNAME=baseline_${DURATION}_allping
+echo ${OUTNAME}
+date
+python3 metameasurement.py $CPUAFF -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$DEST -c "sleep $DURATION" -F ${OUTNAME} -l
+
+echo "done"
