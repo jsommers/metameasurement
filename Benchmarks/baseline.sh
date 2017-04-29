701be2c4a878c338db6c86b48607959bcbdc9faa
Joel Sommers <jsommers@colgate.edu>
2017-04-29 07:57:20 -0400

    pile on to baseline script

--- Benchmarks/baseline.sh
+++ Benchmarks/baseline.sh
@@ -8,7 +8,8 @@ CPUPIN=$(($NCPU-1))
 CPUAFF="-C${CPUPIN}"
 SLEEP=5
 
-for MON in cpu mem io netstat cpu:interval=5 rtt:interface=${INTF}:type=ping:dest=${DEST} rtt:interface=${INTF}:type=hoplimited:maxttl=1:dest=8.8.8.8 ; do
+#for MON in cpu mem io netstat cpu:interval=5 rtt:interface=${INTF}:type=ping:dest=${DEST} rtt:interface=${INTF}:type=hoplimited:maxttl=1:dest=8.8.8.8 ; do
+for MON in cpu cpu:interval=5 rtt:interface=${INTF}:type=ping:dest=${DEST} rtt:interface=${INTF}:type=hoplimited:maxttl=1:dest=8.8.8.8 ; do
     MONNAME=`echo $MON | perl -pe 's/[:\.]/_/g' | perl -pe 's/=//g'`
     OUTNAME=baseline_${DURATION}_${MONNAME}
     echo ${OUTNAME}
@@ -34,6 +35,14 @@ echo ${OUTNAME}
 date
 python3 metameasurement.py $CPUAFF -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=hoplimited:dest=8.8.8.8:maxttl=1 -c "sleep $DURATION" -F ${OUTNAME} -l
 sleep ${SLEEP}
+
+for H in 2 3 4 5; do
+    OUTNAME=baseline_${DURATION}_allhoplimited_${H}hops
+    echo ${OUTNAME}
+    date
+    python3 metameasurement.py $CPUAFF -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=hoplimited:dest=8.8.8.8:maxttl=${H} -c "sleep $DURATION" -F ${OUTNAME} -l
+    sleep ${SLEEP}
+done
 
 OUTNAME=baseline_${DURATION}_allhoplimited_interval5
 echo ${OUTNAME}
