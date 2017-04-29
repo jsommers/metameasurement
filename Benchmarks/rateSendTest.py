fa411e26d16f7aa84eb9e427e34dbbe0aed253e4
Joel Sommers <jsommers@colgate.edu>
2017-04-29 07:51:12 -0400

    make ratesend test a bit more generalized

--- Benchmarks/rateSendTest.py
+++ Benchmarks/rateSendTest.py
@@ -2,15 +2,26 @@
 
 import sys
 from subprocess import run
+import os
 
 ptype="type=ping"
-dest="10.13.13.1"
-interface='bce1'
 xname='ping'
 
-for rate in range(1,501,2):
-    cmd = 'cpuset -c -l7 python3 metameasurement.py -l -Mcpu -Mrtt:interface={}:
-{}:dest={}:rate={} -F send_{}{} -c "sleep 60"'.format(interface, ptype, dest, ra
+if sys.platform == 'linux':
+    cmdprefix = ""
+    dest="10.43.43.3"
+    interface="eth0"
+    cpu = max(os.sched_getaffinity(os.getpid()))
+    cpupin = "-C{}".format(cpu)
+else: # freebsd
+    cmdprefix = "cpuset -c -l7 "
+    cpupin = ""
+    dest="10.13.13.1"
+    interface="bce1"
+
+for rate in range(1,501,4):
+    cmd = '{}python3 metameasurement.py {}-l -Mcpu -Mrtt:interface={}:
+{}:dest={}:rate={} -F send_{}{} -c "sleep 60"'.format(cmdprefix, cpupin, interface, ptype, dest, ra
 te, xname, rate)
     print("Running {}".format(rate))
     print(cmd)
