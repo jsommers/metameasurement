4bf2df7534b51304f5d558990d2358e343ad0796
Joel Sommers <jsommers@colgate.edu>
2017-04-29 07:39:20 -0400

    add 5sec baseline for each type of probe

--- Benchmarks/baseline.sh
+++ Benchmarks/baseline.sh
@@ -2,11 +2,11 @@
 
 INTF=eth0
 DEST=10.42.42.3
-DURATION=3600
+DURATION=900
 NCPU=`cat /proc/cpuinfo | grep '^processor' | wc -l`
 CPUPIN=$(($NCPU-1))
 CPUAFF="-C${CPUPIN}"
-SLEEP=10
+SLEEP=5
 
 for MON in cpu mem io netstat cpu:interval=5 rtt:interface=${INTF}:type=ping:dest=${DEST} rtt:interface=${INTF}:type=hoplimited:maxttl=1:dest=8.8.8.8 ; do
     MONNAME=`echo $MON | perl -pe 's/[:\.]/_/g' | perl -pe 's/=//g'`
@@ -21,5 +21,24 @@ OUTNAME=baseline_${DURATION}_allping
 echo ${OUTNAME}
 date
 python3 metameasurement.py $CPUAFF -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$DEST -c "sleep $DURATION" -F ${OUTNAME} -l
+sleep ${SLEEP}
+
+OUTNAME=baseline_${DURATION}_allping_interval5
+echo ${OUTNAME}
+date
+python3 metameasurement.py $CPUAFF -Mcpu:interval=5 -Mmem:interval=5 -Mio:interval=5 -Mnetstat:interval=5 -Mrtt:interface=$INTF:type=ping:dest=$DEST:rate=0.2 -c "sleep $DURATION" -F ${OUTNAME} -l
+sleep ${SLEEP}
+
+OUTNAME=baseline_${DURATION}_allhoplimited
+echo ${OUTNAME}
+date
+python3 metameasurement.py $CPUAFF -Mcpu -Mmem -Mio -Mnetstat -Mrtt:interface=$INTF:type=ping:dest=$DEST -c "sleep $DURATION" -F ${OUTNAME} -l
+sleep ${SLEEP}
+
+OUTNAME=baseline_${DURATION}_allhoplimited_interval5
+echo ${OUTNAME}
+date
+python3 metameasurement.py $CPUAFF -Mcpu:interval=5 -Mmem:interval=5 -Mio:interval=5 -Mnetstat:interval=5 -Mrtt:interface=$INTF:type=ping:dest=$DEST:rate=0.2 -c "sleep $DURATION" -F ${OUTNAME} -l
+sleep ${SLEEP}
 
 echo "done"
