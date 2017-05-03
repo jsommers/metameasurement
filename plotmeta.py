82065352f35e26c3839ebb12fd2c9eee85950b58
R <ramakrish007@gmail.com>
2017-05-03 14:44:02 -0500

    Square layout fixed, ip field fixed, lines to points fixed

--- plotmeta.py
+++ plotmeta.py
@@ -10,31 +10,31 @@ import matplotlib.pyplot as plt
 from cycler import cycler
 
 def _gather_ts(rawdlist, key):
-    ts = [ ts for ts,_ in rawdlist ] 
+    ts = [ ts for ts,_ in rawdlist ]
     vals = [ xd[key] for ts,xd in rawdlist ]
     return ts, vals
 
 def _gather_ts_rtt(rawdlist, xkey):
     tsval = []
     rtt = []
-    for i in range(len(rawdlist)): 
+    for i in range(len(rawdlist)):
         ts, xd = rawdlist[i]
         rval = xd['rtt']
         if isinf(rval) or rval < 0:
             continue
         tsval.append(ts)
         rtt.append(rval)
-    return tsval, rtt  
+    return tsval, rtt
 
 def plotItems(inbase, datamap, keys):
-    f, axarr = plt.subplots(figsize=(len(keys)*4,len(keys)*4), nrows=len(keys), ncols=1, sharex=True, squeeze=False)
-    color_cycle = cycler(c=[ 'C{}'.format(i) for i in range(3) ])
-    ls_cycle = cycler('ls', ['-.', '--', '-', ':'])
-    lw_cycle = cycler('lw', range(1, 4))
-    sty_cycle = ls_cycle * (color_cycle + lw_cycle)
-    styles = []
-    for i, sty in enumerate(sty_cycle):
-        styles.append(sty)
+    f, axarr = plt.subplots(figsize=(len(keys)*6,len(keys)*4), nrows=len(keys), ncols=1, sharex=True, squeeze=False)
+    # color_cycle = cycler(c=[ 'C{}'.format(i) for i in range(3) ])
+    # ls_cycle = cycler('ls', ['-.', '--', '-', ':'])
+    # lw_cycle = cycler('lw', range(1, 4))
+    # sty_cycle = ls_cycle * (color_cycle + lw_cycle)
+    # styles = []
+    # for i, sty in enumerate(sty_cycle):
+    #     styles.append(sty)
 
     for idx, key in enumerate(keys):
         ts = []
@@ -43,10 +43,18 @@ def plotItems(inbase, datamap, keys):
         dkey = key.split(':')[-1]
         if key.endswith('rtt'):
             ts, data = _gather_ts_rtt(rawdlist, dkey)
+        elif key.endswith('ipsrc'):
+            continue
         else:
             ts, data = _gather_ts(rawdlist, dkey)
 
-        axarr[idx,0].plot(ts, data, label=dkey, **styles[idx])
+        # axarr[idx,0].plot(ts, data, label=dkey, **styles[idx], marker='o')
+        # axarr[idx,0].plot(ts, data, label=dkey, color='C{}'.format(idx))
+
+        maxD = max(data)
+        minD = min(data)
+        axarr[idx,0].scatter(ts, data, label=dkey, color='C{}'.format(idx), marker='o')
+        axarr[idx,0].set_ylim([minD - 0.1 * minD, maxD + 0.1 * maxD])
         axarr[idx,0].grid()
         axarr[idx,0].set_ylabel(dkey)
     axarr[len(keys)-1,0].set_xlabel("Time (s)")
@@ -56,11 +64,11 @@ def plotItems(inbase, datamap, keys):
 
 def plotGroups(inbase, datamap, keyList):
     for k in keyList:
-        matchitems = [] 
+        matchitems = []
         matchkey = "{}:".format(k)
         for dkey in datamap.keys():
             if dkey.startswith(matchkey):
-                matchitems.append(dkey)         
+                matchitems.append(dkey)
         plotItems('_'.join((inbase, k)), datamap, matchitems)
 
 def plotAll(inbase, datamap):
@@ -113,12 +121,12 @@ def _make_key_map(metadata):
 
 def main():
     parser = argparse.ArgumentParser(description='Plotting script - v2')
-    parser.add_argument('-i', '--item', dest='items', action='append', 
+    parser.add_argument('-i', '--item', dest='items', action='append',
                         help='Include individual item in the plot, e.g., monitors:cpu:idle')
-    parser.add_argument('-g', '--group', dest='groups', action='append', 
+    parser.add_argument('-g', '--group', dest='groups', action='append',
                         help='Include a group of items in the plot, e.g., \
                         rtt includes rtt and seq')
-    parser.add_argument('-a', '--all', action='store_true', 
+    parser.add_argument('-a', '--all', action='store_true',
                         help='Plot all groups in separate subplots.')
     parser.add_argument('-l', '--list', action='store_true',
                         help='List all data keys that can be plotted')
